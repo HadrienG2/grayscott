3a6c5fa75c490d001039ccfcd3b79b97b5c376cd
Hadrien G. <knights_of_ni@gmx.com>
2024-01-04 17:35:08 +0100

    Homogeneize error message

--- compute/block/src/default.rs
+++ compute/block/src/default.rs
@@ -31,7 +31,7 @@ impl DefaultBlockSize for SingleCore {
     fn new(topology: &Topology) -> Self {
         let cache_stats = topology
             .cpu_cache_stats()
-            .expect("CPU cache stats aren't known");
+            .expect("CPU cache properties aren't known");
         let cache_sizes = cache_stats.smallest_data_cache_sizes();
 
         let l1_block_size = cache_sizes.first().copied().unwrap_or(32 * 1024) as usize / 2;
